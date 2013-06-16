e322d5f82f99d587cbae993660498794e4d42515
Felipe de Castro <felipepvc@gmail.com>
2013-06-16 01:17:15 -0300

    Frontend finalizado

--- js/main.js
+++ js/main.js
@@ -3,15 +3,49 @@ var Menu = {
     
     //Animação de mouse over do menu principal
     init: function(){
-        $('#topo #menu-superior ul li ul').parent('li').hover(        
-            function(){
-                $('#topo #menu-superior').stop(true,false).animate({height: '136'}, 200, 'easeOutQuart', function(){$('#topo #menu-superior ul li ul').fadeIn(200);}).css('overflow', 'visible');
-            },
-            function(){
-                $('#topo #menu-superior').stop(true,false).animate({height: '68'}, 300, 'easeOutQuint').css('overflow', 'visible');
-                $('#topo #menu-superior ul li ul').fadeOut(100);
-            }
-        );    
+        if($('#topo #menu-superior ul li ul li a').hasClass('active')){
+            $('#topo #menu-superior').css({'height': '136'});
+            $('#topo #menu-superior ul li ul li a.active').parents('ul.submenu').show();
+            $('#topo #menu-superior ul li').hover(        
+                function(){
+                    var este = $(this).find('ul.submenu');
+                    if($(this).find('ul').hasClass('submenu')){
+                        if($(this).find('a').hasClass('active')){}
+                        else{
+                            $('#topo #menu-superior ul li ul li a.active').parents('ul.submenu').fadeOut(200);
+                            $(este).fadeIn(200);                      
+                        }
+                    }
+                    else if($(this).find('a').hasClass('contato')){
+                        $('#topo #menu-superior ul li ul li a.active').parents('ul.submenu').fadeOut(200);
+                    }
+                },
+                function(){
+                    if($(this).find('ul').hasClass('submenu')){
+                        if($(this).find('a').hasClass('active')){}
+                        else{
+                            $('ul.submenu').fadeOut(200);
+                            $('#topo #menu-superior ul li ul li a.active').parents('ul.submenu').fadeIn(200);
+                        }
+                    }
+                    else if($(this).find('a').hasClass('contato')){
+                        $('#topo #menu-superior ul li ul li a.active').parents('ul.submenu').fadeIn(200);
+                    }
+                }
+            );
+        }
+        else{
+            $('#topo #menu-superior ul li ul').parent('li').hover(        
+                function(){
+                    var este = $(this).find('ul.submenu');
+                    $('#topo #menu-superior').stop(true,false).animate({height: '136'}, 200, 'easeOutQuart', function(){$(este).fadeIn(200);}).css('overflow', 'visible');
+                },
+                function(){
+                    $('#topo #menu-superior').stop(true,false).animate({height: '68'}, 300, 'easeOutQuint').css('overflow', 'visible');
+                    $('#topo #menu-superior ul li ul').fadeOut(100);
+                }
+            );
+        }    
     }
 
 }
@@ -106,15 +140,15 @@ var ListaAlunos = {
         );
     },
 
-    //Comportamento no mouse over do resumo dos alunos
+    //Comportamento de mouse over do resumo dos alunos
     resumo: function(){
         $('ul#conteudo li').hover(
             function(){
-                $(this).find('.info').stop(true,false).delay(150).slideDown(600, 'easeOutQuint');
+                $(this).find('.info').stop(true,true).delay(150).slideDown(400, 'easeOutQuint');
                 $(this).find('h4').css({'color':'#fff', 'border-color': '#232323'});
                 $(this).css('background', '#8d1a13');
             }, function(){
-                $(this).find('.info').stop(true,false).delay(100).slideUp(600, 'easeOutQuint');
+                $(this).find('.info').stop(true,true).delay(100).slideUp(400, 'easeOutQuint');
                 $(this).find('h4').css({'color':'#6e6e6e', 'border-color': '#6e6e6e'});
                 $(this).css('background', '#fff');
             }
@@ -123,13 +157,13 @@ var ListaAlunos = {
     }
 }    
 
-//DETALHES DO ALUNO
-var Aluno ={
+//ABAS DE NAVEGAÇÃO
+var Abas ={
 
-    //Abas de navegação
-    navegacaoAbas: function(){
+    init: function(){
 
-        $('#conteudo2, #conteudo3').hide()
+        $('.container').hide();
+        $('#conteudo1').show();
  
         $('ul#abas li a').click(function(e){
             e.preventDefault();
@@ -137,19 +171,105 @@ var Aluno ={
             var target = $(this).attr('href')
 
             $('ul#abas li a').removeClass('active');
-            $('#conteudo1, #conteudo2, #conteudo3').fadeOut(300)
+            $('.container').fadeOut(300)
             $(this).addClass('active');
             $(target).delay(300).fadeIn('fast');
         })
     }
 }
 
- 
+//DEPOIMENTOS
+var Depoimentos = {
+
+    init: function(){
+        $('#detalhes .depoimento:even').addClass('impar');
+
+        $('#detalhes .depoimento').each(function(){
+            var imagemNormal = $(this).attr('rel1');
+            $(this).find('.wrap').css('background-image', 'url(' + imagemNormal + ')');
+        });
+        Depoimentos.efeitoHover();
+        Depoimentos.efeitoClique();
+    },
+
+    efeitoHover: function(){
+        $('#detalhes .depoimento').hover(
+            function(){
+                if($(this).hasClass('aberto')){}
+                else{
+                    var imagemNegativa = $(this).attr('rel2');
+                    $(this).find('.wrap').css({'background-image': 'url(' + imagemNegativa + ')', 'background-position': 'center right'});
+                }
+            },
+            function(){
+                if($(this).hasClass('aberto')){}
+                else{
+                    var imagemNormal = $(this).attr('rel1');
+                    $(this).find('.wrap').css({'background-image': 'url(' + imagemNormal + ')', 'background-position': 'center right'});
+                }
+            }
+        )
+    },
+
+    efeitoClique: function(){
+        $('#detalhes .depoimento').click(function(){
+
+            if($(this).hasClass('aberto')){
+                var imagemNormal = $(this).attr('rel1');
+                $(this).removeClass('aberto');
+                $(this).find('.wrap').find('p').hide();
+                $(this).find('.wrap').css('background-image', 'url(' + imagemNormal + ')');
+            }
+            else{
+                var imagemMaior = $(this).attr('rel3');
+                $('#detalhes .depoimento').removeClass('aberto')
+                $('#detalhes .depoimento').find('.wrap').find('p').hide();
+                $('#detalhes .depoimento').find('.wrap').css('background-image', 'url(' + imagemNormal + ')');        
+                $(this).addClass('aberto');
+                $(this).find('.wrap').find('p').show();
+                $(this).find('.wrap').css('background-image', 'url(' + imagemMaior + ')');
+
+            }
+        });
+    }
+}
+
+//FORMULÁRIO DE CONTATO
+var Formulario = {
+
+    init: function(){
+        $('#detalhes.contato #conteudo form input').focus(function(){
+            msg = $(this).attr('rel');
+            console.log(msg);
+            if ($(this).attr('value') == msg){ $(this).attr('value','')}
+        });
+        $('#detalhes.contato #conteudo form input').blur(function(){
+            msg = $(this).attr('rel');
+            if ($(this).attr('value') == ''){ $(this).attr('value',msg)}
+        });
+        Formulario.mostrarCampos();
+    },
+
+    mostrarCampos: function(){
+        $('#detalhes.contato #conteudo form #opcionais').slideUp();
+        $("[name='caderno']").click(function(){
+            if ($("[name='caderno']:checked").val() == 'sim'){
+                $('#detalhes.contato #conteudo form #opcionais').slideDown();
+            }
+            else{
+                $('#detalhes.contato #conteudo form #opcionais').slideUp();
+            }
+        });
+    }
+}
+
 //Ao carregar todo o documento...
 jQuery(document).ready(function($) {
     Menu.init();
     CarroselDestaques.init();
     ListaAlunos.filtros();
     ListaAlunos.resumo();
-    Aluno.navegacaoAbas();
+    Abas.init();
+    Depoimentos.init();
+    Formulario.init();
 });
